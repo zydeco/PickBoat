a849788b98e8a043cea027e2dec8057df01669d8
Jesús A. Álvarez <zydeco@namedfork.net>
2012-02-25 12:39:50 +0100

    v0.4 use new config/listener API customise boat drops when destroyed

--- src/net/namedfork/bukkit/PickBoat/PickBoat.java
+++ src/net/namedfork/bukkit/PickBoat/PickBoat.java
@@ -1,15 +1,8 @@
 package net.namedfork.bukkit.PickBoat;
 
-import java.io.File;
-import java.io.InputStream;
-import java.io.FileOutputStream;
-import java.util.jar.JarFile;
-import java.util.jar.JarEntry;
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.Configuration;
 
 /**
  * PickBoat for Bukkit
@@ -17,50 +10,21 @@ import org.bukkit.util.config.Configuration;
  * @author Jesús A. Álvarez (zydeco@namedfork.net)
  */
 public class PickBoat extends JavaPlugin {
-    public boolean boatsDieWhenDestroyed, boatsDieWhenCrashed, boatsReturnToOwner, boatsReturnToAttacker, boatsNeverCrash;
 
     public void onEnable() {
         PluginDescriptionFile pdfFile = this.getDescription();
         System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
         
-        // load configuration
-        this.loadConfigFile();
-        Configuration cfg = this.getConfiguration();
-        boatsDieWhenDestroyed = cfg.getBoolean("boats_die_when_destroyed", false);
-        boatsDieWhenCrashed = cfg.getBoolean("boats_die_when_crashed", false);
-        boatsReturnToOwner = cfg.getBoolean("boats_return_to_owner", false);
-        boatsReturnToAttacker = cfg.getBoolean("boats_return_to_attacker", false);
-        boatsNeverCrash = cfg.getBoolean("boats_never_crash", false);
-
+        this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
         
         PickBoatBoatListener boatListener = new PickBoatBoatListener(this);
-        boatListener.registerEvents();
+        getServer().getPluginManager().registerEvents(boatListener, this);
     }
     
     public void onDisable() {
         PluginDescriptionFile pdfFile = this.getDescription();
         System.out.println("Disabling " + pdfFile.getName());
     }
-
-    public void loadConfigFile() {
-		// load config file, creating it first if it doesn't exist
-		File configFile = new File(this.getDataFolder(), "config.yml");
-		if (!configFile.canRead()) try {
-			configFile.getParentFile().mkdirs();
-			JarFile jar = new JarFile(this.getFile());
-			JarEntry entry = jar.getJarEntry("config.yml");
-			InputStream is = jar.getInputStream(entry);
-			FileOutputStream os = new FileOutputStream(configFile);
-			byte[] buf = new byte[(int)entry.getSize()];
-			is.read(buf, 0, (int)entry.getSize());
-			os.write(buf);
-			os.close();
-			this.getConfiguration().load();
-		} catch (Exception e) {
-			System.out.println("PickBoat: could not create configuration file");
-		}
-
-
-	}
 }
 
--- src/net/namedfork/bukkit/PickBoat/PickBoatBoatListener.java
+++ src/net/namedfork/bukkit/PickBoat/PickBoatBoatListener.java
@@ -1,34 +1,85 @@
 package net.namedfork.bukkit.PickBoat;
 
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Set;
 import org.bukkit.*;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.util.Vector;
 import org.bukkit.event.vehicle.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.entity.*;
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.material.MaterialData;
 
-public class PickBoatBoatListener extends VehicleListener {
+public class PickBoatBoatListener implements Listener {
     private final PickBoat plugin;
 
     public PickBoatBoatListener(final PickBoat plugin) {
         this.plugin = plugin;
     }
     
-    public void registerEvents() {
-        PluginManager pm = plugin.getServer().getPluginManager();
-        pm.registerEvent(Event.Type.VEHICLE_DESTROY, this, Priority.High, plugin);
-    }
-    
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onVehicleDestroy(VehicleDestroyEvent event) {
         if (!(event.getVehicle() instanceof Boat)) return;
         Boat boat = (Boat) event.getVehicle();
         Entity attacker = event.getAttacker();
-        if (plugin.boatsDieWhenCrashed && attacker == null) return;
-        if (plugin.boatsDieWhenDestroyed && attacker != null) return;
+        if ((plugin.getConfig().getBoolean("boats_die_when_crashed") && attacker == null) ||
+                (plugin.getConfig().getBoolean("boats_die_when_destroyed") && attacker != null)) {
+            // remove boat
+            boat.remove();
+            event.setCancelled(true);
+            
+            // drop things
+            ConfigurationSection drops = null;
+            try {
+                drops = (ConfigurationSection)plugin.getConfig().get("boat_drop");
+            } catch (Exception e) {
+                System.out.println("[PickBoat] using default boat_drop");
+                drops = (ConfigurationSection)plugin.getConfig().getDefaults().get("boat_drop");
+            }
+            
+            Set<String> dropKeys = drops.getKeys(false);
+            Iterator<String> i = dropKeys.iterator();
+            while(i.hasNext()) {
+                String rawKey = i.next();
+                String key[] = rawKey.toUpperCase(Locale.ENGLISH).split(":");
+                
+                // find material
+                Material m = Material.AIR;
+                try {
+                    m = Material.getMaterial(Integer.parseInt(key[0]));
+                } catch (NumberFormatException e) {
+                    m = Material.getMaterial(key[0]);
+                }
+                
+                // find value
+                int value = 0;
+                if (key.length >= 2) try {
+                    value = Integer.parseInt(key[1]);
+                } catch (NumberFormatException e) {
+                    value = 0;
+                }
+                
+                // find amount
+                int amount = drops.getInt(rawKey);
+                
+                // drop
+                if (m != null && amount > 0) {
+                    MaterialData md = new MaterialData(m, (byte)value);
+                    Location loc = boat.getLocation();
+                    for(int a=0; a < amount; a++) {
+                        loc.getWorld().dropItemNaturally(loc, md.toItemStack(1));
+                    }
+                }
+            }
+            
+            return;
+        }
         
-        if (plugin.boatsNeverCrash && attacker == null) {
+        if (plugin.getConfig().getBoolean("boats_never_crash") && attacker == null) {
             // boat crashed
             boat.setVelocity(new Vector(0,0,0));
             event.setCancelled(true);
@@ -41,9 +92,9 @@ public class PickBoatBoatListener extends VehicleListener {
         
         // find out who gets the boat
         Player boatReceiver = null;
-        if (plugin.boatsReturnToOwner && boat.getPassenger() instanceof Player) {
+        if (plugin.getConfig().getBoolean("boats_return_to_owner") && boat.getPassenger() instanceof Player) {
             boatReceiver = (Player) boat.getPassenger();
-        } else if (plugin.boatsReturnToAttacker && attacker instanceof Player) {
+        } else if (plugin.getConfig().getBoolean("boats_return_to_attacker") && attacker instanceof Player) {
             boatReceiver = (Player) attacker;
         }
         
